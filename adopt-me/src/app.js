b2e7cd48b490609bfa7a594ee7567c3657d8cea2
Christian Foster <fosterchris000@gmail.com>
2021-08-24 18:17:48 -0400

    Added strict mode

--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 // import Pet from "./Pet";
 import SearchParams from "./SearchParams";
@@ -38,4 +38,9 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
